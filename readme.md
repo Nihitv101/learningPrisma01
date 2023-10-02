6732b80cfd0f3346113b022d4f2a8967c07c72d6
Nihit Verma <nihitv2003@gmail.com>
2023-10-03 00:50:19 +0530

    setting up database

---
+++ readme.md
@@ -0,0 +1,1 @@
+Using Prisma ORM (Object relational model)
--- server.js
+++ server.js
@@ -1,4 +1,19 @@
+import dotenv from 'dotenv';
 import express from 'express';
 
-const app = express();
+require('dotenv');
 
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.get('/', (req, res)=>{
+    return res.send("hi")
+})
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`server isr running in port ${PORT}`);
+})
